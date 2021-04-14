d42e18c47c988671588c3a4727979de2a59d778d
Michael Eliachevitch <meliache@uni-bonn.de>
2021-04-15 00:56:59 +0200

    Add CLI exercises

---
+++ exercises/exercise01/zen.py
@@ -0,0 +1,6 @@
+"""
+Script to be executed as an exercise in using python to execute file.
+``import this`` is a python easter egg which prints the zen of python.
+"""
+if __name__ == '__main__':
+    import this

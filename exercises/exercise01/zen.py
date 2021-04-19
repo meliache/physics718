d46ea785884cf63954449e8069b26871a55ad97f
Michael Eliachevitch <meliache@uni-bonn.de>
2021-04-19 22:42:19 +0200

    Ignore pylint error for `import this`

--- exercises/exercise01/zen.py
+++ exercises/exercise01/zen.py
@@ -3,4 +3,4 @@ Script to be executed as an exercise in using python to execute file.
 ``import this`` is a python easter egg which prints the zen of python.
 """
 if __name__ == '__main__':
-    import this
+    import this  # pylint: disable=W0611

4e9e566f297d82a325f03159b33ac228ccf251f8
Michael Eliachevitch <meliache@uni-bonn.de>
2021-04-20 07:22:28 +0200

    Add first homework exercise

---
+++ exercises/exercise01/exercise01_homework.py
@@ -0,0 +1,24 @@
+import unittest
+
+# if you want you can re-use the is_odd function from the attendance exercise
+from exercise01_attendance import is_odd
+
+
+def calc_odd_fibonacci_numbers(n_numbers):
+    """
+    Return a list of the first N odd fibonacci numbers
+    """
+    pass
+
+
+# Optionally: Use a helper function that returns fibonacci numbers and use that
+# in you main function that calculates the odd fibonacci numbers
+def calc_n_fibonacci_numbers(n_numbers):
+    """
+    Helper function that returns fibonacci numbers
+    """
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main(module="test_exercise01_homework", verbosity=2)
